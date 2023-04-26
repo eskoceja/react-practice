0287a50928afc399917bca44a7bc2322e02c7b7a
Emily Koceja <emilysiquina@Emilys-Air.localdomain>
2023-04-26 15:37:21 -0400

    first commit

---
+++ src/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increment() {
+    setCount(count + 1);
+  }
+
+  function decrement() {
+    setCount(count - 1);
+  }
+
+  return (
+    <>
+      <h1>Count: {count}</h1>
+      <>
+        <button onClick={increment}>Increment Button</button>
+        <button onClick={decrement}>Decrement Button</button>
+      </>
+    </>
+  );
+}
